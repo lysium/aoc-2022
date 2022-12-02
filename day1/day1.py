0d2da7c3eaeee694df6bad2ab611ecaf1da69c13
Dr. Andreas Bernauer <andreas.bernauer@jambit.com>
2022-12-02 11:15:06 +0100

    solve day1, part2

--- day1/day1.py
+++ day1/day1.py
@@ -1,19 +1,23 @@
 
 def main(file):
     f = open(file, "r")
-    max_calories = 0
+    max_calories = [0, 0, 0]
     elf_calories = 0
 
     for line in f.readlines():
         line = line.replace("\n", "")
         if line == "":
-            max_calories = max(max_calories, elf_calories)
+            for i in range(3):
+                if elf_calories >= max_calories[i]:
+                    old_max = max_calories[i]
+                    max_calories[i] = elf_calories
+                    elf_calories = old_max
             elf_calories = 0
             pass
         else:
             calories = int(line)
             elf_calories += calories
-    print(max_calories)
+    print(max_calories, sum(max_calories))
 
 
 if __name__ == "__main__":
