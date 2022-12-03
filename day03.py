883020baa63311962d0db7a8f0aed9583e551c43
Dr. Andreas Bernauer <andreas.bernauer@jambit.com>
2022-12-03 14:19:47 +0100

    solve day3 part2

--- day03.py
+++ day03.py
@@ -10,10 +10,31 @@ def item_priority(item):
 
 
 def main(file):
+    part1(file)
+    part2(file)
+
+
+def part2(file):
+    total_badge_prio = 0
+    group_items = []
+    for line in read_file_lines(file):
+        group_items.append(set(line))
+        if len(group_items) == 3:
+            badge_item = group_items[0].intersection(group_items[1]).intersection(group_items[2])
+            if len(badge_item) > 1:
+                raise "too many common items"
+            badge_item_prio = item_priority(badge_item.pop())
+            total_badge_prio += badge_item_prio
+            group_items = []
+
+    print(total_badge_prio)
+
+
+def part1(file):
     total_prio = 0
     for line in read_file_lines(file):
         no_items = len(line)
-        compartment1, compartment2 = line[0:no_items//2], line[no_items//2:]
+        compartment1, compartment2 = line[0:no_items // 2], line[no_items // 2:]
         items1 = set(compartment1)
         items2 = set(compartment2)
         common_items = items1.intersection(items2)
