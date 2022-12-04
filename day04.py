b879226a26e888a19a26961127287cf500b7e3ee
Dr. Andreas Bernauer <andreas.bernauer@jambit.com>
2022-12-04 07:08:31 +0100

    solve day4, part2

--- day04.py
+++ day04.py
@@ -9,12 +9,17 @@ def main(file):
 
 class Assignment:
     def __init__(self, start, end):
+        assert start <= end
         self.start = start
         self.end = end
 
     def contains(self, other):
         """true if this assignment fully contains the other"""
         return self.start <= other.start and self.end >= other.end
+
+    def overlaps(self, other):
+        """true if this assignment overlaps (or contains) the other"""
+        return not(self.end < other.start or other.end < self.start)
 
 
 def assignment_from_string(pair):
@@ -23,14 +28,19 @@ def assignment_from_string(pair):
 
 
 def part1(file):
-    count = 0
+    covers = 0
+    overlaps = 0
     for line in read_file_lines(file):
         (pair1, pair2) = line.split(',')
         assignment1 = assignment_from_string(pair1)
         assignment2 = assignment_from_string(pair2)
         if assignment1.contains(assignment2) or assignment2.contains(assignment1):
-            count +=1
-    print(count)
+            covers += 1
+        if assignment1.overlaps(assignment2):
+            overlaps += 1
+
+    print(covers)
+    print(overlaps)
 
 
 def part2(file):
