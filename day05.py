f552269ab157f49327a5b360b5144dd15b9aacb0
Dr. Andreas Bernauer <andreas.bernauer@jambit.com>
2022-12-05 11:25:50 +0100

    refactor into logical parts

--- day05.py
+++ day05.py
@@ -2,6 +2,7 @@ import sys
 from util import read_file_lines
 from enum import Enum
 import re
+from dataclasses import dataclass
 
 
 def main(file):
@@ -9,15 +10,18 @@ def main(file):
     part2(file)
 
 
-def crate_mover_9001(crates, amount, start, end):
-    return crate_mover_helper(crates, amount, start, end, byOne=False)
+def crate_mover_9001(crates, instruction):
+    return crate_mover_helper(crates, instruction, byOne=False)
 
 
-def crate_mover_9000(crates, amount, start, end):
-    return crate_mover_helper(crates, amount, start, end, byOne=True)
+def crate_mover_9000(crates, instruction):
+    return crate_mover_helper(crates, instruction, byOne=True)
 
 
-def crate_mover_helper(crates, amount, start, end, byOne):
+def crate_mover_helper(crates, instruction, byOne):
+    amount = instruction.amount
+    start = instruction.stack_start
+    end = instruction.stack_end
     moved_crates = crates[start][-amount:]
     crates[start] = crates[start][:-amount]
     if byOne:
@@ -26,47 +30,75 @@ def crate_mover_helper(crates, amount, start, end, byOne):
     return crates
 
 
-class State(Enum):
-    READ_CRATES = 1,
-    READ_INSTRUCTIONS = 2
-
-
 def execute_input(file, crate_mover):
-    state = State.READ_CRATES
-    crates = []
-    crate_lines = []
-    for line in read_file_lines(file):
-        if state == State.READ_CRATES:
-            if line == "":
-                stack_labels = re.split('\s+', crate_lines[-1])[1:-1]
-                no_stacks = len(stack_labels)
-                max_stack_height = len(crate_lines) - 1
-                for x in range(no_stacks):
-                    stack = []
-                    for y in range(max_stack_height):
-                        cargo = crate_lines[y][1+4*x]
-                        if cargo != ' ':
-                            stack.append(cargo)
-                    stack.reverse()
-                    crates.append(stack)
-                state = State.READ_INSTRUCTIONS
-            else:
-                crate_lines.append(line)
-        elif state == State.READ_INSTRUCTIONS:
-            m = re.match("move (\d+) from (\d+) to (\d+)", line)
-            if not m:
-                print(f"no match: {line}")
-                raise Exception()
-            amount = int(m.group(1))
-            stack_start = int(m.group(2)) - 1
-            stack_end = int(m.group(3)) - 1
-            crate_mover(crates, amount, stack_start, stack_end)
+    f = open(file, 'r')
+    crates = read_crates(f)
+    for instruction in read_instructions(f):
+        crates = crate_mover(crates, instruction)
+    result = top_cargo(crates)
+    print(result)
 
+
+def read_crates(file):
+    crate_lines = read_crate_lines(file)
+    return crates_from_crate_lines(crate_lines)
+
+
+def crates_from_crate_lines(crate_lines):
+    crates = []
+    stack_labels = re.split('\s+', crate_lines[-1])[1:-1]
+    no_stacks = len(stack_labels)
+    max_stack_height = len(crate_lines) - 1
+    for x in range(no_stacks):
+        stack = []
+        for y in range(max_stack_height):
+            cargo = crate_lines[y][1+4*x]
+            if cargo != ' ':
+                stack.append(cargo)
+        stack.reverse()
+        crates.append(stack)
+    return crates
+
+
+@dataclass
+class Instruction:
+    amount: int
+    stack_start: int
+    stack_end: int
+
+
+def read_instructions(f):
+    while True:
+        line = f.readline()
+        if line == '':
+            break
+
+        m = re.match("move (\d+) from (\d+) to (\d+)", line)
+        if not m:
+            print(f"no match: {line}")
+            raise Exception()
+        amount = int(m.group(1))
+        stack_start = int(m.group(2)) - 1
+        stack_end = int(m.group(3)) - 1
+        yield Instruction(amount, stack_start, stack_end)
+
+
+def top_cargo(crates):
     result = ""
     for crate in crates:
         if len(crate) > 0:
             result += crate[-1]
-    print(result)
+    return result
+
+
+def read_crate_lines(file):
+    crate_lines = []
+    while True:
+        line = file.readline().strip('\n')
+        if line == "":
+            break
+        crate_lines.append(line)
+    return crate_lines
 
 
 def part1(file):
