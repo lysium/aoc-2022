90a42fedc612f63bbe1843df66351ca0046366df
Dr. Andreas Bernauer <andreas.bernauer@jambit.com>
2022-12-06 09:17:22 +0100

    add timing, for curiosity
    my timings (for 14, 5000 rounds):
    - plain: 0.0027 secs/round
    - dict:  0.0048 secs/round
    - list:  0.0049 secs/round
    
    `python3 day06.py input06.txt plain 5000`

--- day06.py
+++ day06.py
@@ -1,29 +1,38 @@
 import sys
 from enum import Enum
-from util import read_file_lines
+import timeit
 
 
 def main(argv):
     day = 6
     input_file = f"input{day:02}.txt"
     method = Method.PLAIN
+    times = 0
     if len(argv) >= 2:
         input_file = sys.argv[1]
     if len(argv) >= 3:
         method = Method(sys.argv[2])
+    if len(argv) >= 4:
+        times = int(sys.argv[3])
 
     if method == Method.PLAIN:
         execute = execute_plain
     elif method == Method.DICT:
         execute = execute_with_dict
     elif method == Method.LIST:
-        pass
-        #execute = execute_with_list
+        execute = execute_with_list
     else:
         raise Exception("unknown method")
 
     print(execute(input_file, 4))
     print(execute(input_file, 14))
+
+    if times > 0:
+        duration = timeit.timeit(f'{execute.__name__}("{input_file}", 14)',
+                                 setup=f"from __main__ import {execute.__name__}",
+                                 number=times)
+        print(f'duration: {duration}')
+        print(f'duration/times: {duration/times}')
 
 
 def all_different(buffer):
@@ -69,6 +78,27 @@ def execute_with_dict(file, no_distinct):
                 return no_chars
             else:
                 seen[buffer[0]] -= 1
+                buffer = buffer[1:]
+        no_chars += 1
+
+
+def execute_with_list(file, no_distinct):
+    f = open(file, 'r')
+    no_chars = 1
+    buffer = []
+    ord_a = ord('a')
+    seen = [0 for _ in range(ord('z') - ord_a + 1)]
+    while True:
+        c = f.read(1)
+        if c == '':
+            raise Exception('nothing found')
+        buffer.append(c)
+        seen[ord(c) - ord_a] += 1
+        if len(buffer) == no_distinct:
+            if all([count <= 1 for count in seen]):
+                return no_chars
+            else:
+                seen[ord(buffer[0]) - ord_a] -= 1
                 buffer = buffer[1:]
         no_chars += 1
 
