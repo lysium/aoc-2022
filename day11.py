ffad65fc7b08bea92c15cefa2e7006b28dbe75cf
Dr. Andreas Bernauer <andreas.bernauer@jambit.com>
2022-12-11 17:16:03 +0100

    tidy up a bit

--- day11.py
+++ day11.py
@@ -1,8 +1,6 @@
 from __future__ import annotations
 import sys
-from util import read_file_lines
 from dataclasses import dataclass
-from anytree import NodeMixin
 import os
 import re
 import operator
@@ -40,6 +38,15 @@ class Test:
     def __repr__(self):
         return f"Test[{self.test} ? {self.yes_monkey} : {self.no_monkey}]"
 
+    @staticmethod
+    def from_lines(test_l, test_no_l, test_yes_l):
+        pattern = "(\d+)"
+        test_int = int(re.search(pattern, test_l).group(1))
+        test_yes = int(re.search(pattern, test_yes_l).group(1))
+        test_no = int(re.search(pattern, test_no_l).group(1))
+        test = Test(test_int, test_yes, test_no)
+        return test
+
 
 def main(file):
     part1(file)
@@ -47,19 +54,19 @@ def main(file):
 
 
 def parse_operation(line):
-    """line is like 'new = old * 19'; return corresponding operation"""
-    l = len("new = ")
-    arg1, op_sym, arg2 = line[l:].split(' ')
+    """line is like '  Operation: new = old * 19'; return corresponding operation"""
+    offset = len("  Operation: new = ")
+    arg1, op_sym, arg2 = line[offset:].split(' ')
     op = operator.mul if op_sym == "*" else operator.add
     op_str = "multiply" if op_sym == "*" else "add"
     if arg1 == "old" and arg2 == "old":
-        return Operation(lambda old: op(old, old), f"{op_str} both: " + line)
+        return Operation(lambda old: op(old, old), f"{op_str} both")
     elif arg1 == "old":
         arg2 = int(arg2)
-        return Operation(lambda old: op(old, arg2), f"{op_str} {arg2}: " + line)
+        return Operation(lambda old: op(old, arg2), f"{op_str} {arg2}")
     elif arg2 == "old":  # ... actually never happens in input11.txt
         arg1 = int(arg1)
-        return Operation(lambda old:op(arg1, old), f"arg {op_sym} old: " + line)
+        return Operation(lambda old: op(arg1, old), f"arg {op_sym} old")
     else:
         raise Exception(f"cannot parse operation line {line}")
 
@@ -78,26 +85,13 @@ def part1(file):
     counts = [0 for _ in monkeys]
     for round in range(20):
         for monkey in monkeys:
-            debug(f"Monkey {monkey.name}:")
             for item in monkey.items:
                 counts[int(monkey.name)] += 1
-                debug(f"  Monkey inspects an item with a worry level of {item}.")
                 new_item = monkey.op.do(item)
-                debug(f"    Worry level is {monkey.op.descr} to {new_item}.")
                 new_item = new_item // 3
-                debug(f"    Monkey gets bored with item. Worry level is divided by 3 to {new_item}.")
                 target_monkey = monkey.test.yes_monkey if new_item % monkey.test.test == 0 else monkey.test.no_monkey
-                debug(f"    Item with worry level 500 is thrown to monkey {target_monkey}.")
                 monkeys[target_monkey].items.append(new_item)
             monkey.items = []
-        debug(f"After round {round}:")
-        for monkey in monkeys:
-            debug(f"  Monkey {monkey.name}: {monkey.items}")
-            pass
-
-    for monkey, count in enumerate(counts):
-        debug(f"Monkey {monkey}: {count}")
-        pass
     max1_count, max2_count = max2(counts)
     print(max1_count * max2_count)
 
@@ -110,7 +104,7 @@ def part2(file):
         max_div *= monkey.test.test
 
     counts = [0 for _ in monkeys]
-    for round in range(1,10_000+1):
+    for round in range(1, 10_000+1):
         for monkey in monkeys:
             for item in monkey.items:
                 counts[int(monkey.name)] += 1
@@ -132,35 +126,19 @@ def part2(file):
 
 def read_monkeys_from_file(file):
     monkeys = []
-    monkey = None
-    for line in read_file_lines(file):
-        if line.startswith("Monkey"):
-            l = len("Monkey ")
-            name = int(line[l:l + 1])
-        elif line.startswith("  Starting items:"):
-            l = len("  Starting items:")
-            items = [int(i) for i in (line[l:]).split(", ")]
-        elif line.startswith("  Operation: "):
-            l = len("  Operation: ")
-            operation = parse_operation(line[l:])
-        elif line.startswith("  Test: "):
-            l = len("  Test: divisible by ")
-            test_int = int(line[l:])
-        elif line.startswith("    If true:"):
-            l = len("    If true: throw to monkey ")
-            test_yes = int(line[l:])
-        elif line.startswith("    If false:"):
-            l = len("    If false: throw to monkey ")
-            test_no = int(line[l:])
-        elif line == "":
-            test = Test(test_int, test_yes, test_no)
-            monkey = Monkey(name, items, operation, test)
-            # print(monkey)
-            monkeys.append(monkey)
-    test = Test(test_int, test_yes, test_no)
-    monkey = Monkey(name, items, operation, test)
-    # print(monkey)
-    monkeys.append(monkey)
+    for monkey_lines in open(file, 'r').read().split('\n\n'):
+        [monkey_l, items_l, op_line, test_l, test_yes_l, test_no_l] = monkey_lines.split('\n')[0:6]
+
+        name = int(monkey_l[-2:-1])
+
+        offset = len("  Starting items:")
+        items = [int(i) for i in (items_l[offset:]).split(", ")]
+
+        operation = parse_operation(op_line)
+        test = Test.from_lines(test_l, test_no_l, test_yes_l)
+        monkey = Monkey(name, items, operation, test)
+        # print(monkey)
+        monkeys.append(monkey)
     return monkeys
 
 
