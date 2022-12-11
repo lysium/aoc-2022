fcac1f4ab2839a155566d4b6fdbb594e764c4dec
Dr. Andreas Bernauer <andreas.bernauer@jambit.com>
2022-12-11 08:25:28 +0100

    solve day11, part2

--- day11.py
+++ day11.py
@@ -128,8 +128,72 @@ def part1(file):
 
 
 def part2(file):
+    monkeys = []
+    monkey = None
     for line in read_file_lines(file):
-        pass
+        if line.startswith("Monkey"):
+            l = len("Monkey ")
+            name = int(line[l:l+1])
+        elif line.startswith("  Starting items:"):
+            l = len("  Starting items:")
+            items = [int(i) for i in (line[l:]).split(", ")]
+        elif line.startswith("  Operation: "):
+            l = len("  Operation: ")
+            operation = parse_operation(line[l:])
+        elif line.startswith("  Test: "):
+            l = len("  Test: divisible by ")
+            test_int = int(line[l:])
+        elif line.startswith("    If true:"):
+            l = len("    If true: throw to monkey ")
+            test_yes = int(line[l:])
+        elif line.startswith("    If false:"):
+            l = len("    If false: throw to monkey ")
+            test_no = int(line[l:])
+        elif line == "":
+            test = Test(test_int, test_yes, test_no)
+            monkey = Monkey(name, items, operation, test)
+            # print(monkey)
+            monkeys.append(monkey)
+    test = Test(test_int, test_yes, test_no)
+    monkey = Monkey(name, items, operation, test)
+    # print(monkey)
+    monkeys.append(monkey)
+
+    max_div = 1
+    for monkey in monkeys:
+        max_div *= monkey.test.test
+
+    counts = [0 for _ in monkeys]
+    for round in range(1,10_000+1):
+        for monkey in monkeys:
+            # print(f"Monkey {monkey.name}:")
+            for item in monkey.items:
+                counts[int(monkey.name)] += 1
+                # print(f"  Monkey inspects an item with a worry level of {item}.")
+                new_item = monkey.op.do(item)
+                # print(f"    Worry level is {monkey.op.descr} to {new_item}.")
+                new_item = new_item % max_div
+                # print(f"    Monkey gets bored with item. Worry level is divided by 3 to {new_item}.")
+                target_monkey = monkey.test.yes_monkey if new_item % monkey.test.test == 0 else monkey.test.no_monkey
+                # print(f"    Item with worry level 500 is thrown to monkey {target_monkey}.")
+                monkeys[target_monkey].items.append(new_item)
+                if target_monkey == int(monkey.name):
+                    raise "Self-referrential"
+            monkey.items = []
+        if round == 1 or round == 20 or round % 1_000 == 0:
+            print(f"== After round {round} ==")
+            for monkey, count in enumerate(counts):
+                print(f"Monkey {monkey}: {count}")
+
+            for monkey in monkeys:
+                # print(f"  Monkey {monkey.name}: {monkey.items}")
+                pass
+
+    max_count = max(counts)
+    counts.remove(max_count)
+    max2_count = max(counts)
+    print(max_count * max2_count)
+
 
 if __name__ == "__main__":
     this_script_name = os.path.basename(sys.argv[0])
