fd01e428e0c80dc46fc8f5a7b13ab5e9bc7a8448
Dr. Andreas Bernauer <andreas.bernauer@jambit.com>
2022-12-12 10:57:23 +0100

    use dataclass features to reduce #methods in Pos

--- pos.py
+++ pos.py
@@ -1,27 +1,13 @@
 from __future__ import annotations
 
 from dataclasses import dataclass
-from functools import total_ordering
 from vec import Vec
 
 
-@dataclass
-@total_ordering
+@dataclass(frozen=True, eq=True, order=True, repr=True)
 class Pos:
     x: int
     y: int
-
-    def __hash__(self):
-        return 1009 * self.x + self.y
-
-    def __eq__(self, other):
-        return self.x == other.x and self.y == other.y
-
-    def __lt__(self, other):
-        return self.x < other.x and self.y < other.y
-
-    def __repr__(self):
-        return f"({self.x},{self.y})"
 
     def __add__(self, other: Vec):
         return Pos(self.x + other.dx, self.y + other.dy)
