677e5c0386be25be54c34d346efceb8ba76913ef
Dr. Andreas Bernauer <andreas.bernauer@jambit.com>
2022-12-14 07:04:45 +0100

    solve day14, part2

--- day14.py
+++ day14.py
@@ -63,8 +63,48 @@ def part1(file):
 
 
 def part2(file):
+    lines_of_rock = []
     for line in read_file_lines(file):
-        pass
+        points = [Pos(int(x), int(y)) for x,y in [xy.split(',') for xy in line.split(' -> ')]]
+        lines_of_rock.append(points)
+    print(lines_of_rock)
+    abyss_y = max([max([pos.y for pos in line]) for line in lines_of_rock])
+    # print(abyss_y)
+
+    rock = set()
+    for line in lines_of_rock:
+        for start_pos, end_pos in zip(line, line[1:]):
+            if start_pos.x == end_pos.x: # horizontal line
+                vec = DOWN if start_pos.y < end_pos.y else UP
+            elif start_pos.y == end_pos.y:  # vertical line
+                vec = RIGHT if start_pos.x < end_pos.x else LEFT
+            else:
+                raise Exception("not a straight line")
+            rock.add(start_pos)
+            pos = start_pos
+            while pos != end_pos:
+                pos += vec
+                rock.add(pos)
+
+    count = 0
+    SW = Vec(-1, 1)
+    SE = Vec(1, 1)
+    while not Pos(500,0) in rock:
+        sand = Pos(500,0)
+        while True:
+            new_pos = sand + DOWN
+            if new_pos in rock or new_pos.y >= abyss_y + 2:
+                new_pos = sand + SW
+                if new_pos in rock or new_pos.y >= abyss_y + 2:
+                    new_pos = sand + SE
+                    if new_pos in rock or new_pos.y >= abyss_y + 2:
+                        rock.add(sand)
+                        count = count + 1
+                        break
+            sand = new_pos
+    print(count)
+
+
 
 
 def input_file_from_argv(argv):
