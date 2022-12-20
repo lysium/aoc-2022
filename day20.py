ab20f9dd4a72136c5ca01b1eb544e3e8f1491b4b
Dr. Andreas Bernauer <andreas.bernauer@jambit.com>
2022-12-20 17:41:05 +0100

    refactor part1, part2

--- day20.py
+++ day20.py
@@ -12,8 +12,15 @@ def debug(args):
         print(args)
 
 def main(file):
-    part1(file)
-    part2(file)
+    print(part1(file))
+    print(part2(file))
+
+
+def part1(file):
+    return decrypt(file, 1, 1)
+
+def part2(file):
+    return decrypt(file, 811589153, 10)
 
 class Number:
     def __init__(self, n:int, prev:Number = None, next:Number = None):
@@ -41,78 +48,14 @@ def print_numbers(start:Number):
         number = number.next
     print()
 
-def part1(file):
-    numbers = [Number(int(line)) for line in read_file_lines(file)]
-    length = len(numbers)
-    for i in range(0, length):
-        numbers[i].prev = numbers[i-1]
-        numbers[i].next = numbers[(i+1) % length]
-
-    if do_debug:
-        print("Numbers              : ", end='')
-        for number in numbers:
-            print(number.n, end=', ')
-        print()
-        print("Numbers (off 2)      : ", end='')
-        print_numbers(numbers[2])
-        remove(numbers[1])
-        print("Numbers (rm1, off 2) : ", end='')
-        print_numbers(numbers[2])
-        insert_after(numbers[1], numbers[0])
-        print("Numbers (ns1, off 2) : ", end='')
-        print_numbers(numbers[2])
-
-        print("Initial arrangement:")
-        print_numbers(numbers[0])
-
-    for number in numbers:
-        steps = number.n
-        insert_here = number.prev
-        remove(number)
-        if steps > 0:
-            for i in range(steps):
-                insert_here = insert_here.next
-            debug(f"{number.n} moves between {insert_here.n} and {insert_here.next.n}")
-            insert_after(number, insert_here)
-            if do_debug:
-                print_numbers(numbers[0])
-        elif steps < 0:
-            for i in range(-steps):
-                insert_here = insert_here.prev
-            debug(f"{number.n} moves between {insert_here.n} and {insert_here.next.n}")
-            insert_after(number, insert_here)
-            if do_debug:
-                print_numbers(numbers[0])
-        elif steps == 0:
-            debug(f"0 does not move:")
-            insert_after(number, insert_here)
-            if do_debug:
-                print_numbers(numbers[0])
-            pass
-
-    zero = numbers[0]
-    while zero.n != 0:
-        zero = zero.next
-    coordinates = []
-    for _ in range(3):
-        for _ in range(1000):
-            zero = zero.next
-        coordinates.append(zero.n)
-    print(coordinates, sum(coordinates))
-
-
-
-
-
-def part2(file):
-    decryption_key = 811589153
+def decrypt(file, decryption_key, rounds):
     numbers = [Number(int(line) * decryption_key) for line in read_file_lines(file)]
     length = len(numbers)
     for i in range(0, length):
         numbers[i].prev = numbers[i - 1]
         numbers[i].next = numbers[(i + 1) % length]
 
-    for _ in range(10):
+    for _ in range(rounds):
         for number in numbers:
             steps = number.n
             insert_here = number.prev
@@ -146,7 +89,8 @@ def part2(file):
         for _ in range(1000):
             zero = zero.next
         coordinates.append(zero.n)
-    print(coordinates, sum(coordinates))
+    debug(coordinates)
+    return sum(coordinates)
 
 
 def input_file_from_argv(argv):
