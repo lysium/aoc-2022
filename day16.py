067dc3a43aa1ca7b1b858594a7e08b193b54c803
Dr. Andreas Bernauer <andreas.bernauer@jambit.com>
2022-12-21 07:14:27 +0100

    more WIP for day16...

--- day16.py
+++ day16.py
@@ -1,5 +1,7 @@
 from __future__ import annotations
 import sys
+import time
+
 from util import read_file_lines
 from dataclasses import dataclass
 from anytree import NodeMixin
@@ -63,10 +65,68 @@ def play(valve, valves, actions):
 class Trait:
     actions: [Action]
     valve: Valve
-    open_valves: set(Valve)
+    open_valves: set[Valve]
+
+
+def find_shortest_path(valves, start, end):
+    if start == end:
+        return []
+    trails = [[tunnel] for tunnel in valves[start].tunnels_to]
+    while True:
+        trail = trails.pop(0)
+        for tunnel in valves[trail[-1]].tunnels_to:
+            if tunnel == end:
+                return trail
+            trails.append(trail + [tunnel])
+
+def test_find_shortest_path():
+    valves = read_valves_from_file('test16.txt')
+    assert (find_shortest_path(valves, 'AA', 'AA')) == ['AA']
+    assert (find_shortest_path(valves, 'AA', 'DD')) == ['AA', 'DD']
+    assert (find_shortest_path(valves, 'AA', 'CC')) == ['AA', 'DD', 'CC']
+    assert (find_shortest_path(valves, 'DD', 'CC')) == ['DD', 'CC']
 
 
 def part1(file):
+    valves = read_valves_from_file(file)
+
+    non_zero_rate_valves = set([valve.id for valve in valves.values() if valve.rate != 0])
+    valve_ids = [key for key in non_zero_rate_valves]
+    shortest_paths = {}
+    start_time = time.monotonic()
+    for i in range(len(valve_ids)):
+        for j in range(i, len(valve_ids)):
+            start = valve_ids[i]
+            end = valve_ids[j]
+            shortest_path = find_shortest_path(valves, start, end)
+            shortest_paths.setdefault(start, {})[end] = shortest_path
+            shortest_paths.setdefault(end, {})[start] = shortest_path
+    print(f'took {time.monotonic() - start_time:.1f} seconds')
+
+
+    pos = 'AA'
+    open_valves = set()
+    remaining_minutes = 30
+    while remaining_minutes > 0:
+        # how far are the valves that make sense to open?
+        distances = [(valve, len(shortest_paths[pos][valve])+1) for valve in non_zero_rate_valves]
+        # remove those that are not reachable in remaining minutes
+        reachable = [(valve, distance) for (valve, distance) in distances if distance < remaining_minutes]
+        sorted(reachable, key=lambda x: x[1], reverse=True)
+        best_valve = sorted[0]
+        # what is the rate I can get there for the remaining time?
+        # it takes {distance}-1 minutes to get there, plus + 1 to open valve
+        # so the additional rate is (remaining_minutes - distance) * valve.rate
+        rates = [(valve, distance, (remaining_minutes - distance) * valves[valve].rate) for valve, distance in reachable]
+        # what is the max I get?
+        valve, distance, max_rate = max(rates, key=lambda x: x[2])
+        print(valve, distance, max_rate)
+        open_valves.add(valve)
+        non_zero_rate_valves.remove(valve)
+        remaining_minutes -= distance
+
+
+def read_valves_from_file(file):
     pattern = 'Valve (\w+) has flow rate=(\d+); tunnels? leads? to valves? ([A-Z, ]+)'
     valves = {}
     for line in read_file_lines(file):
@@ -81,6 +141,11 @@ def part1(file):
         else:
             raise Exception(f'no match for "{line}"')
         pass
+    return valves
+
+
+def dont_do():
+    valves = []
     valve = 'AA'
     traits = [Trait([MoveTo(v)], v, set()) for v in valves[valve].tunnels_to]
     while len(traits[0].actions) < 30:
